22761accb23d037ff2c1a7579f2c79baca55ecf0
Reeha Pradhan <pradhanreeha@gmail.com>
2020-12-11 13:54:23 +0545

    [ADD] Make function to show added todolist

--- WT/lab3/todo-application.js
+++ WT/lab3/todo-application.js
@@ -13,6 +13,27 @@ function addTodo(text) {
 
     // Let's check todoList array in the console
     console.log(todoList);
+
+    // Let's show the added todoList
+    showTodoList(todoTask);
+}
+
+// Show the todo lists which had been added from the HTML form
+function showTodoList(todoTask) {
+    const todoList = document.querySelector('.js-todolist');
+    const isTicked = todoTask.ticked ? 'ticked' : '';
+    const liNode = document.createElement('li');
+    liNode.setAttribute('class', 'todo-task ${isTicked}');
+    liNode.setAttribute('data-key', todoTask.id);
+    liNode.innerHTML = `
+        <input id="${todoTask.id}" type="checkbox"/>
+        <label for="${todoTask.id}" class="ticklist js-ticklist"></label>
+        <span>${todoTask.text}</span>
+        <button class="delete-todo-task js-delete-todo-task">
+            <i class="fa fa-close"></i>
+        </button>
+    `;
+    todoList.append(liNode);
 }
 
 // Let's select the form element 
@@ -20,10 +41,10 @@ const todolistForm = document.querySelector('.todolist-form');
 
 // Listen to the submit event
 todolistForm.addEventListener('submit', event => {
-    
+
     // Let's prevent the page to refresh after submitting the data from the form.
     event.preventDefault();
-    
+
     // Let's get input data from the form
     const inputTodoListData = document.querySelector('.todolist-input');
 
