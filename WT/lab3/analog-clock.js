af8ac23d8dd958e51d55a13449aa218fd8d5465b
Reeha Pradhan <pradhanreeha@gmail.com>
2020-12-12 18:44:22 +0545

    [IMP] Execute clock start every 1000ms

--- WT/lab3/analog-clock.js
+++ WT/lab3/analog-clock.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
-drawClock();
+setInterval(drawClock, 1000);
 
 function drawClock() {
   drawFace(ctx, radius);
