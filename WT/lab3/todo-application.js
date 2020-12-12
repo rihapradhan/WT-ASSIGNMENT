8da517ffbe214213bdaf84e6edf4bece9cbd0947
Reeha Pradhan <pradhanreeha@gmail.com>
2020-12-12 10:25:24 +0545

    [ADD] Toggle tick action if ticked

--- WT/lab3/todo-application.js
+++ WT/lab3/todo-application.js
@@ -1,7 +1,7 @@
 // Let's make an array to store todo list items
-let todoList = [];
+let todoListArray = [];
 
-// Let's make a function to create todo list object and assign to todoList array
+// Let's make a function to create todo list object and assign to todoListArray array
 function addTodo(text) {
     const todoTask = {
         id: Date.now(),
@@ -9,31 +9,55 @@ function addTodo(text) {
         ticked: false,
     };
 
-    todoList.push(todoTask);
+    todoListArray.push(todoTask);
 
-    // Let's check todoList array in the console
-    console.log(todoList);
+    // Let's check todoListArray array in the console
+    console.log(todoListArray);
 
     // Let's show the added todoList
     showTodoList(todoTask);
 }
 
+function toggleTicked(key) {
+    const index = todoListArray.findIndex(item => item.id === Number(key));
+    todoListArray[index].ticked = !todoListArray[index].ticked;
+    showTodoList(todoListArray[index]);
+  }
+
 // Show the todo lists which had been added from the HTML form
 function showTodoList(todoTask) {
+    // Get todo list task
     const todoList = document.querySelector('.js-todolist');
+
+    console.log(todoTask.id);
+
+    // Get current todo list task
+    const currentTask = document.querySelector(`[data-key="${todoTask.id}"]`);
+
     const isTicked = todoTask.ticked ? 'ticked' : '';
     const liNode = document.createElement('li');
-    liNode.setAttribute('class', 'todo-task ${isTicked}');
+    liNode.setAttribute('class', `todo-task ${isTicked}`);
     liNode.setAttribute('data-key', todoTask.id);
     liNode.innerHTML = `
-        <input id="${todoTask.id}" type="checkbox"/>
-        <label for="${todoTask.id}" class="ticklist js-ticklist"></label>
-        <span>${todoTask.text}</span>
-        <button class="delete-todo-task js-delete-todo-task">
-            <i class="fa fa-close"></i>
-        </button>
+        <input id="${todoTask.id}" type="checkbox" class="ticklist js-ticklist"/>
+        <label for="${todoTask.id}"></label>
+        <span class="todo-task-text">${todoTask.text}</span>
+        <button class="delete-todo-task js-delete-todo-task">Delete</button>
     `;
-    todoList.append(liNode);
+    if (todoTask.ticked) {
+        // Get input node from li node which is in the 0th position
+        const inputNode = liNode.children[0];
+        inputNode.setAttribute("checked", "checked");
+    }
+
+    // Replace the current task it is already existed else append to the end of the list
+    console.log(currentTask);
+    if (currentTask) {
+        todoList.replaceChild(liNode, currentTask);
+
+    } else {
+        todoList.append(liNode);
+    }
 }
 
 // Let's select the form element 
@@ -54,5 +78,18 @@ todolistForm.addEventListener('submit', event => {
     // Let's check if there is text data and add to the todoList array.
     if (textData != '') {
         addTodo(textData);
+        inputTodoListData.value = '';
+        inputTodoListData.focus();
     }
+});
+
+// Take all the todo list
+const todolist = document.querySelector('.js-todolist');
+
+// Listen to click event of todolist children
+todolist.addEventListener('click', event => {
+  if (event.target.classList.contains('js-ticklist')) {
+    const todoListKey = event.target.parentElement.dataset.key;
+    toggleTicked(todoListKey);
+  }
 });
