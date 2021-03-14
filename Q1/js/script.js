73b1a861ab81b19abea905ccb794533da2e33a6c
rihapradhan <75767843+rihapradhan@users.noreply.github.com>
2021-03-14 22:57:41 +0545

    Delete Q1 directory

--- Q1/js/script.js
+++
@@ -1,9 +0,0 @@
-let input = prompt("Enter a number: ");
-
-if (isNaN(input)) {
-  console.log("INVALID INPUT");
-} else if (input % 2 == 0) {
-  console.log("The number is even!");
-} else if (input % 0 != 0) {
-  console.log("The number is odd!");
-}
