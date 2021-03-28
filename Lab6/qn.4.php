b9529e31522e7ecef65408da4655e7fb7fbb56e0
rihapradhan <75767843+rihapradhan@users.noreply.github.com>
2021-03-28 21:53:35 +0545

    Delete Lab6 directory

--- Lab6/qn.4.php
+++
@@ -1,4 +0,0 @@
-<?php
-$name = "ree";
-echo strlen($name);
-?>
--- Lab6/qn3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$age = array("ree"=>"18", "biju"=>"25", "pallu"=>"8");
-
-foreach($age as $x => $x_value) {
-  echo "Key=" . $x . ", Value=" . $x_value;
-  echo "<br>";
-}
-?>
